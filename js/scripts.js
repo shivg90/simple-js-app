e688721756be9d152f9b6d69065482144efd0948
shivg90 <118476977+shivg90@users.noreply.github.com>
2022-12-08 14:05:13 +0100

    added array of objects to pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
-
+let pokemonList = [
+    { name: 'charmeleon', height: 1.1, type: 'fire' },
+    { name: 'pikachu', height: 0.4, type: 'electric' },
+    { name: 'diglett', height: 0.2, type: 'ground' },
+  ]
 
