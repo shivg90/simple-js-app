fdd3e1ee24fb2d7af5c5558fe8c77d706688f814
shivg90 <118476977+shivg90@users.noreply.github.com>
2022-12-14 19:17:45 +0100

    added IIFE and functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
-/* defined pokemonList as variable and added 3 key-values to differentiate each object in the array */
-let pokemonList = [
-    { name: 'charmeleon', height: 1.1, type: 'fire' },
-    { name: 'pikachu', height: 0.4, type: 'electric' },
-    { name: 'diglett', height: 0.2, type: 'ground' },
-  ]
+/* defined new variable and wrapped in IIFE to eliminate code from global use*/
+let pokemonRepository = (function(){
+  /* defined pokemonList as variable and added 3 key-values to differentiate each object in the array */
+  let pokemonList = [
+      { name: 'charmeleon', height: 1.1, type: 'fire' },
+      { name: 'pikachu', height: 0.4, type: 'electric' },
+      { name: 'diglett', height: 0.2, type: 'ground' },
+    ]
+    
+      function add(pokemon) {
+      pokemonList.push(pokemon);
+    }
+      function getAll() {
+        return pokemonList;
+      }
+  
+      return {
+        add: add,
+        getAll: getAll
+      };
+  
+  })();
+  
+  console.log(pokemonRepository.getAll());
+  
+  pokemonRepository.add({ name: 'Bulbasaur', height: 0.7, type: 'grass'});
 
+  
 // /* OLD CODE */
 // /* tell 'for-loop' code to look to length of variable array above first */
 // for (let i=0; i < pokemonList.length; i++){
@@ -21,7 +42,8 @@ let pokemonList = [
 //   }
 //   }
 
-pokemonList.forEach(function(pokemon) {
+/* forEach loop uses pokemonRepository.getAll() instead of pokemonList to retrieve data now that array is protected by IIFE*/
+pokemonRepository.getAll().forEach(function(pokemon) {
   if (pokemon.height >= 1) {
     document.write(pokemon.name + " " + " (height: " + pokemon.height + ") - Wow, that's big!" + "<br>");
   }else {
